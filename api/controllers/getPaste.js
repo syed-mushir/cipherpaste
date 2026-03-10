26b954e26f8244c428ca2cf4c8c36de39a169a86
Syed Mushir <mushir.work@gmail.com>
2026-03-10 14:23:23 +0530

    Add getPaste api controller

--- api/controllers/getPaste.js
+++ api/controllers/getPaste.js
@@ -1,3 +1,21 @@
+import { getValue } from "../config/redis.js"
+import { AppError } from "../utils/errorHandler.js";
+
 export const getPaste = async (req, res) => {
+    const { id } = req.params;
+    if(!id?.trim()){
+        throw new AppError("Invalid paste id")
+    }
+
+    const paste = await getValue(id);
+
+    if(!paste){
+        throw new AppError("Paste not found", 404)
+    }
     
+    res.json({
+        status: true,
+        message: "Paste fetched successfully",
+        cipherText: paste
+    })
 }
