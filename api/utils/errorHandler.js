04ea8012e9921000a651be3db227ac18b975a910
Syed Mushir <mushir.work@gmail.com>
2026-03-10 14:22:11 +0530

    Add a global error handler

---
+++ api/utils/errorHandler.js
@@ -0,0 +1,11 @@
+export class AppError extends Error {
+  constructor(message, statusCode = 400) {
+    super(message)
+    this.name = "AppError";
+    this.statusCode = statusCode;
+    
+    if(Error.captureStackTrace){
+        Error.captureStackTrace(this, AppError);
+    }
+  }
+}
