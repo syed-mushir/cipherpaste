331d197e426c6c6cc7c71e1d2b7ddac611e2dafe
Syed Mushir <mushir.work@gmail.com>
2026-03-10 14:25:25 +0530

    Add view.html to display paste if it exists

--- api/server.js
+++ api/server.js
@@ -3,20 +3,27 @@ import { PORT } from "./config/env.js";
 import apiRoutes from "./routes/api.routes.js";
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { checkPaste } from "./controllers/checkPaste.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
+app.use(express.json());
 app.use("/api", apiRoutes);
+app.use("/:id", checkPaste(path.join(__dirname, '..' ,'web','view.html')))
 app.use(express.static(path.join(__dirname, '..' ,'web')))
 app.use((req,res) => {
-    res.status(404).sendFile(path.join(__dirname,'..','web','404.html'));
+    res.sendFile(path.join(__dirname,'..','web','404.html'));
 })
 
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).json({status: false, message: 'Something broke!'})
+    if(err.name == "AppError"){
+        res.status(err.statusCode).json({status: false, message: err.message});
+        return;
+    }
+    console.error(err.stack)
+    res.status(500).json({status: false, message: 'Internal Server Error'})
 })
 
 
